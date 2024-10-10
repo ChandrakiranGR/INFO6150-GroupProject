9c4216d33865b09a828d10d39a2992957ebfd53b
PradyumnaRaghavendra <raghavendra.p@northeastern.edu>
2024-10-10 16:45:33 -0400

    in

---
+++ INFO6150-GroupProject-main/INFO6150-GroupProject-main/adOnWheels/frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import LandingPage from "./pages/landingpage/LandingPage";
+import Navbarr from './pages/navbar/Navbarr';
+import Login from './pages/login/Login';
+import Footer from './footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Home from "./pages/home/home";
+import Signup from "./pages/signup/Signup";
+const App = () => {
+  return (
+    <Router>
+      <Navbarr />
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup></Signup>} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+};
+export default App;
