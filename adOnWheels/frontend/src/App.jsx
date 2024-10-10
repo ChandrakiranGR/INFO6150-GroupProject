4adc366b061b615b092536ac6c5e4c14b000dd01
Chandrakiran Gr <guthavariramesh.c@northeastern.edu>
2024-10-09 21:40:43 -0400

    Added Footer, resolved conflicts

--- adOnWheels/frontend/src/App.jsx
+++ adOnWheels/frontend/src/App.jsx
@@ -1,25 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import LandingPage from "./pages/landingpage/LandingPage";
 import Navbarr from "./pages/navbar/Navbarr";
 import Footer from './footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './pages/login/Login.css';
-import Login from './pages/login/Login'
 import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Login />
-      <Footer/>
-      <Navbarr />
-      <LandingPage />
+    <Navbarr />
+    <LandingPage />
+    <Footer/>
     </>
   );
-
+  }
 export default App;
