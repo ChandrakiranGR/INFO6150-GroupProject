555caf39f255a643f63cccc158d84868a4b22166
revan1010 <chonnad.r@northeastern.edu>
2024-10-09 20:43:47 -0400

    Added the navbar and lading v1 files

--- adOnWheels/frontend/src/App.jsx
+++ adOnWheels/frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
 import LandingPage from "./pages/landingpage/LandingPage";
+import Navbarr from "./pages/navbar/Navbarr";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbarr />
       <LandingPage />
     </>
   );
