29cf582ee8886582edef0f8d1920deaf6a3b7551
revan1010 <chonnad.r@northeastern.edu>
2024-12-03 22:53:40 -0500

    Redux setup on auth

--- adOnWheels/frontend/src/pages/navbar/Navbarr.jsx
+++ adOnWheels/frontend/src/pages/navbar/Navbarr.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo1.png";
-
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../redux/slices/authSlice";
 const Navbarr = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   return (
     <Navbar
       bg="dark"
@@ -25,12 +31,28 @@ const Navbarr = () => {
         <Navbar.Collapse id="basic-navbar-nav" className="ms-3 ">
           <Nav className="ms-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link as={Link} to="/login" className="btn-login">
-              Login
-            </Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/">Services</Nav.Link>
+            <Nav.Link href="/">Pricing</Nav.Link>
+            {isAuthenticated ? (
+              // <Nav.Link
+              //   role="button"
+              //   onClick={handleLogout}
+              //   className="btn-login"
+              // >
+              //   Logout
+              // </Nav.Link>
+              <button
+                onClick={handleLogout}
+                className="btn btn-danger ms-2" // Styling for the button
+              >
+                Logout
+              </button>
+            ) : (
+              <Nav.Link as={Link} to="/login" className="btn-login">
+                Login
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
